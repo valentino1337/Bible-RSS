dddee0ac4ab55900fab7d251350b4ceacd5f6eab
valentino1337 <9332943-valentino1337@users.noreply.gitlab.com>
2021-10-23 22:03:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bible RSS
+A simple program that I schedule to run every morning Crontab on my VPS.\
+Puts a random Bible quote into the RSS feed I specify.\
+Uses the KJV version.
+
+## Dependencies
++ `coreutils`
++ `sed`
---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+CHOSENBOOK=`ls KJV | shuf -n 1`
+BOOKTITLE=`head -n 1 KJV/$CHOSENBOOK |cut -d "-" -f 1`
+
+VERSE=`shuf -n 1 KJV/"$CHOSENBOOK"`
+VERSENUM=`echo $VERSE |cut -d "]" -f 1 |cut -d "[" -f 2`
+
+TITLE="$BOOKTITLE$VERSENUM"
+QUOTATION=`echo $VERSE |cut -d "]" -f 2`
+
+echo $TITLE
+echo $QUOTATION
+
+head -n 11 dailyverse.xml > temp.xml
+
+echo "
+<item>
+<title>$TITLE</title>
+<pubDate>`date`</pubDate>
+<description><![CDATA[<p>$QUOTATION</p>]]></description>
+</item>" >> temp.xml
+
+sed '1,/<!-- VALKUNXYZ -->/d' dailyverse.xml >> temp.xml
+
+mv temp.xml dailyverse.xml
